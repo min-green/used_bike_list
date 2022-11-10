e5dc959cb7e58b72013e8fc4458a08db8c4457df
min-green <117242423+min-green@users.noreply.github.com>
2022-11-10 18:01:31 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,81 +5,91 @@ import 'package:http/http.dart' as http;
 import 'package:html/dom.dart' as dom;
 import 'package:html/parser.dart' as parser;
 
-void main() async{
-  test();
-//  runApp(const MyApp());
+main() async {
+  await test();
+  runApp(const MyApp());
 }
 
-void test() async{
+const String _doSsaUrl = 'corearoadbike.com';
+List<Map<String, dynamic>> _keywords = [];
 
-  //var url = Uri.https('corearoadbike.com', '/board/board.php?t_id=Menu01Top6');
-  //var url = Uri.https('corearoadbike.com/board/?g_id=recycle02');
-  var url = Uri.https('corearoadbike.com', '/board/board.php', {'t_id':'Menu01Top6'});
+void Init() {
+  _keywords = [];
+  test();
+}
+
+test() async {
+  var url = Uri.https(_doSsaUrl, '/board/board.php', {'t_id': 'Menu01Top6'});
 
 // Await the http get response, then decode the json-formatted response.
-  // var response = await http.get(url);
   var response = await http.get(url);
   print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
 
   dom.Document documnet = parser.parse(response.body);
 
   var keywordElements = documnet.querySelectorAll('table');
-  List<Map<String, dynamic>> keywords = [];
-  keywordElements[91].querySelector('.list_content_B')?.nodes.toString();
-  for (var element in keywordElements){
-    var link1 = element.querySelectorAll('.list_title_B');
-    var link2 = element.querySelectorAll('.list_content_B');
-    var link3 = element.querySelectorAll('.list_content_B > img');
 
-    // var link = element.querySelector('.list_title_B');
-    // var rank = element.querySelector('.list_content_B');
-    // var img = element.querySelector('.list_content_B > img');
+  List<dom.Element?> lstTable = [];
 
-    // keywords.add({
-    //   'rank': rank!.text,
-    //   'url' : link!.attributes['href'],
-    //   'img' : img!.attributes['src']
-    // });
+  for (var element in keywordElements) {
+    if (element.querySelector('.list_title_B') != null &&
+        element.querySelector('.list_content_B') != null &&
+        element.querySelector('.list_title_B')?.nodes.length == 7) {
+      lstTable.add(element);
 
-    // print(keywords);
+      var contentTxt =
+          element.querySelector('.list_content_B')?.nodes[0].toString();
+      var strFirstIndex = contentTxt?.indexOf('1. 판매');
+      var strSecondIndex = contentTxt?.indexOf('2. 사이즈');
+      var strThirdIndex = contentTxt?.indexOf('3. 사용기간');
+      var strFourthIndex = contentTxt?.indexOf('4. 물건상세설명');
+
+      String? price, size, useDate, detailDesc;
+
+      if (strFirstIndex == -1 || strSecondIndex == -1) {
+        price = '';
+      } else {
+        price = contentTxt?.substring(strFirstIndex!, strSecondIndex);
+      }
+
+      if (strSecondIndex == -1 || strThirdIndex == -1) {
+        size = '';
+      } else {
+        size = contentTxt?.substring(strSecondIndex!, strThirdIndex);
+      }
+
+      if (strThirdIndex == -1 || strFourthIndex == -1) {
+        useDate = '';
+      } else {
+        useDate = contentTxt?.substring(strThirdIndex!, strFourthIndex);
+      }
+
+      if (strFourthIndex == -1) {
+        detailDesc = '';
+      } else {
+        detailDesc = contentTxt?.substring(strFourthIndex!);
+      }
+
+      var strTime = element
+          .querySelector('tr > .list_title_B')
+          ?.parentNode
+          ?.nodes[3]
+          .nodes[1]
+          .toString();
+
+      _keywords.add({
+        'img': element.querySelector('img')?.attributes['src'],
+        'url': element.querySelector('a')?.attributes['href'],
+        'price': price,
+        'size': size,
+        'use_date': useDate,
+        'detail_desc': detailDesc,
+        'time': strTime
+            ?.substring(strTime.indexOf('|') + 1, strTime.lastIndexOf('|'))
+            .trim()
+      });
+    }
   }
-
-  // if (response.statusCode == 200) {
-  //   // 만약 서버가 OK 응답을 반환하면, JSON을 파싱합니다.
-  //   return Post.fromJson(json.decode(response.body));
-  // } else {
-  //   // 만약 응답이 OK가 아니면, 에러를 던집니다.
-  //   throw Exception('Failed to load post');
-  // }
-//  print(await http.read(Uri.https('example.com', 'foobar.txt')));
-}
-
-// class Post {
-//   final int userId;
-//   final int id;
-//   final String title;
-//   final String body;
-//
-//   Post({required this.userId,required this.id,required this.title,required this.body});
-//
-//   factory Post.fromJson(Map<String, dynamic> json) {
-//     return Post(
-//       userId: json['userId'],
-//       id: json['id'],
-//       title: json['title'],
-//       body: json['body'],
-//     );
-//   }
-// }
-
-void add_person() {
-  people.add(Person(10, "q", true));
-  people.add(Person(20, "w", false));
-  people.add(Person(30, "e", false));
-  people.add(Person(40, "r", false));
-  people.add(Person(50, "t", true));
-  people.add(Person(60, "y", true));
 }
 
 class MyApp extends StatelessWidget {
@@ -107,29 +117,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
-var people = [];
+class BikeTile extends StatelessWidget {
+  BikeTile(this._bikeInfo);
 
-class Person {
-  int age;
-  String name;
-  bool isLeftHand;
-
-  Person(this.age, this.name, this.isLeftHand);
-}
-
-class PersonTile extends StatelessWidget {
-  PersonTile(this._person);
-
-  final Person _person;
+  final Map<String, dynamic> _bikeInfo;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Image.network(
-          "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/1YN0/image/ak-gRe29XA2HXzvSBowU7Tl7LFE.png"),
-      title: Text(_person.name),
-      subtitle: Text("${_person.age}세"),
-      trailing: PersonHandIcon(_person.isLeftHand),
+     leading: Image.network('https://' + _doSsaUrl + _bikeInfo['img']),
+      title: Text(_bikeInfo['price']),
+      subtitle: Text(_bikeInfo['detail_desc']),
+      //trailing: PersonHandIcon(_bikeInfo.isLeftHand),
     );
   }
 }
@@ -166,54 +165,14 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class HeaderTile extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Image.network(
-          "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/1YN0/image/ak-gRe29XA2HXzvSBowU7Tl7LFE.png"),
-    );
-  }
-}
-
-class ExplicitListConstructing2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        PersonTile(people[0]),
-        PersonTile(people[1]),
-        PersonTile(people[2]),
-        PersonTile(people[3]),
-        PersonTile(people[4]),
-        PersonTile(people[5]),
-      ],
-    );
-  }
-}
-
-class UsingBuilderListConstructing extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(8),
-      itemCount: people.length,
-      itemBuilder: (BuildContext context, int index) {
-        return PersonTile(people[index - 1]);
-      },
-    );
-  }
-}
-
 class UsingseparatedBuilderListConstructing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
         padding: const EdgeInsets.all(8),
-        itemCount: people.length,
+        itemCount: _keywords.length,
         itemBuilder: (BuildContext context, int index) {
-          return PersonTile(people[index]);
+          return BikeTile(_keywords[index]);
         },
         separatorBuilder: (BuildContext context, int index) {
           return const Divider();
@@ -221,55 +180,10 @@ class UsingseparatedBuilderListConstructing extends StatelessWidget {
   }
 }
 
-class ExplicitListConstructing extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        Container(
-          height: 50,
-          color: Colors.amber[600],
-          child: const Center(child: Text('Entry A')),
-        ),
-        Container(
-          height: 50,
-          color: Colors.amber[500],
-          child: const Center(child: Text('Entry B')),
-        ),
-        Container(
-          height: 50,
-          color: Colors.amber[100],
-          child: const Center(child: Text('Entry C')),
-        ),
-      ],
-    );
-  }
-}
-
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    add_person();
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -280,26 +194,26 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: UsingseparatedBuilderListConstructing(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-        ),
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+      floatingActionButton: const FloatingActionButton(
+        onPressed: Init,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Icon(Icons.refresh),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
